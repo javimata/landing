e6140511f9273ef0a872210970fad3ab9215db6e
Javier Mata <adview@iMac-de-JAM.local>
2018-04-26 13:56:34 -0500

    update

--- php/process.php
+++ php/process.php
@@ -5,35 +5,25 @@ ini_set("display_errors", 1);
 date_default_timezone_set("America/Mexico_City");
 
 //CONFIGURACION:
-$to  = "briani@adview.mx";
-$cc  = "islas@adview.mx";
+$to  = "";
+$cc  = "";
 $bcc = "";
 $fecha = date('Y-m-d H:i:s');
-
-if ($_REQUEST["email"]=="javimata@gmail.com" || $_REQUEST['test'] != "" ) { 
-	$to        = "javimata@gmail.com"; 
-	$cc        = "";
-	$bcc       = "";
-}
 
 $a = $_REQUEST["a"];	
 
 if ($a==1){
 	
 	$nombre   = $_REQUEST["nombre"];
-	$essocio  = $_REQUEST["essocio"];
-	$nosocio  = $_REQUEST["nosocio"];
 	$email    = $_REQUEST["email"];
 	$telefono = $_REQUEST["telefono"];
 	$ciudad   = $_REQUEST["ciudad"];
 
 	
-	$subject = "Solicitud de información - Caja Popular Mexicana";
+	$subject = "Solicitud de información";
 	$body = "
 	Datos del solicitante: <br><br>
 	Nombre: $nombre<br>
-	¿Es socio?: $essocio<br>
-	No. socio: $nosocio<br>
 	E-Mail: $email <br>
 	Telefono: $telefono<br>
 	Ciudad: $ciudad<br>
@@ -61,7 +51,7 @@ if ($a==1){
 	$mail->CharSet = 'UTF-8';
 
 
-	$mail->setFrom("noreply@cpm.coop", "Caja Popular Mexicana - Landing", 0);
+	$mail->setFrom("noreply@cpm.coop", "Landing", 0);
 	$mail->addReplyTo($email,$nombre);
 	$mail->addAddress($to, $nombre);
 
@@ -84,35 +74,12 @@ if ($a==1){
 
 	}
 
-
-	
 }else{
 
 	$responder = 3;
 	$texto_respuesta = "Ha ocurrido un error en el envio.";
 
 }
-
-
-	/*
-	$header = "";
-	if (filter_var($email, FILTER_VALIDATE_EMAIL)) {
-		$header  = "Reply-To: ".$email."\r\n";
-	}
-	$header .= "From: Rotoplas <noreply@rotoplaslabs.com>\r\n";
-	$header .= "Content-type: text/plain; charset=iso-8859-1\r\n";
-	if ($cc!=""){ $header .= "Cc: ".$cc."\r\n"; }
-	if ($bcc!=""){ $header .= "Bcc: ".$bcc."\r\n"; }
-
-	if (mail($to, $subject, utf8_decode($body), $header)){
-		$responder = 1;
-	} else {
-		$responder = 2;
-		$texto_respuesta = "Ha ocurrido un error en el envio " . error_get_last()['message'];
-	}
-	*/
-
-
 	
 //RESPUESTA DE CONSULTA
 	$resp = array(
